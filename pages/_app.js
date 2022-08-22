473339da3fc3214309a6c085e0e13b4d5948e031
Nevin Tay <nevintay@gmail.com>
2022-08-22 14:27:56 +0800

    feat: wrap all components in global Container component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
+import Container from '../components/UI/Layout/Container';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
