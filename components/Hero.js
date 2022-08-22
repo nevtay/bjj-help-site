77d2cbd1ac29278eb92bd61c04318212ba40ab34
Nevin Tay <nevintay@gmail.com>
2022-08-22 14:27:13 +0800

    feat: replace Container component with fragment

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import Container from './UI/Layout/Container'
 
 const Hero = () => {
     return (
-        <Container>
+        <>
             <h1 className='w-4/6 text-white text-6xl leading-normal mb-5'>A Brazilian Jiu-Jitsu Resource Page</h1>
             <span>
                 <button type="button" className="rounded-m inline-flex items-center justify-center text-yellow-500 font-bold w-auto mr-7 hover:underline" aria-expanded="false">
@@ -13,7 +12,7 @@ const Hero = () => {
                     Submissions
                 </button>
             </span>
-        </Container>
+        </>
     )
 }
 
