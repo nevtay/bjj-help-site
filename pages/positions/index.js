14a4cca2b58a6442ecbd94955d754ece4aa49531
Nevin Tay <nevintay@gmail.com>
2022-08-22 14:30:26 +0800

    feat: update text of page

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -1,6 +1,8 @@
 const Positions = () => {
     return (
-        <div>Positions</div>
+        <>
+            <h1 className='w-4/6 text-white text-6xl leading-normal mb-5 text-yellow-500'>Positions</h1>
+        </>
     )
 }
 
