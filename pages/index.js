01a2d202b573055b180fd668d346790be5801957
Nevin Tay <nevintay@gmail.com>
2022-08-22 14:19:06 +0800

    feat: add Container component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import 'tailwindcss/tailwind.css';
+
+import Hero from "../components/Hero"
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>A BJJ Help Site</title>
         <meta name="description" content="Learn BJJ moves, maneuvers, and much more" />
       </Head>
-    </div>
+      <Hero />
+    </>
   )
 }
