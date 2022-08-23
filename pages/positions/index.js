39451e3bbc62c9836dc34b4a0fa304d6f7507ec3
Nevin Tay <nevintay@gmail.com>
2022-08-23 14:21:23 +0800

    feat: import and render positions names

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -1,10 +1,15 @@
 import BackButton from "../../components/UI/BackButton"
+import List from "../../components/UI/Layout/List"
+
+import data from "../../utils/POSITIONS.json"
 
 const Positions = () => {
+    const positions = data.positions
     return (
         <>
             <h1 className='w-4/6 text-white text-6xl leading-normal mb-5 text-yellow-500'>Positions</h1>
             <BackButton hoverStyling="hover:bg-orange-400" />
+            <List data={positions} />
         </>
     )
 }
