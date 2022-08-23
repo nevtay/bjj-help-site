936658031eb74b9ecd27167ce4ed0a7e654d9b63
Nevin Tay <nevintay@gmail.com>
2022-08-23 15:25:26 +0800

    feat: add padding

--- components/UI/Layout/ListItem.js
+++ components/UI/Layout/ListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ListItem = (props) => {
     const item = props.item || ""
     return (
-        <div className='text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-10 px-5 text-center border rounded-full'>
+        <div className='text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-10 px-5 py-1 text-center border rounded-full'>
             <p>{item}</p>
         </div>
     )
