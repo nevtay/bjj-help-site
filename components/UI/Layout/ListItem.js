42a55c596a503db7c28749cabe5ec569f5796600
Nevin Tay <nevintay@gmail.com>
2022-08-23 16:04:59 +0800

    feat: convert each text item to a <Link /> component

--- components/UI/Layout/ListItem.js
+++ components/UI/Layout/ListItem.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import Link from 'next/link'
 
 const ListItem = (props) => {
     const item = props.item || ""
+    const type = props.type || ""
     return (
         <div className='text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-7 md-5 px-5 py-1 text-center border rounded-full'>
-            <p>{item}</p>
+            <Link href={`/${type}/${item}`}>{item}</Link>
         </div>
     )
 }
