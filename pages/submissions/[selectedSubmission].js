8b5094f255371075a3c4399495e3942508a1213e
Nevin Tay <nevintay@gmail.com>
2022-08-23 16:06:22 +0800

    feat: add placeholder code

---
+++ pages/submissions/[selectedSubmission].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectedSubmission = () => {
+    return (
+        <div className='text-white'>[selectedSubmission]</div>
+    )
+}
+
+export default SelectedSubmission
