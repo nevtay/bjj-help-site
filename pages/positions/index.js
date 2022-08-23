632204ac016e5efc53e34e73eff70e29727f2401
Nevin Tay <nevintay@gmail.com>
2022-08-23 13:54:24 +0800

    feat: import BackButton component into page

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -1,7 +1,10 @@
+import BackButton from "../../components/UI/BackButton"
+
 const Positions = () => {
     return (
         <>
             <h1 className='w-4/6 text-white text-6xl leading-normal mb-5 text-yellow-500'>Positions</h1>
+            <BackButton hoverStyling="hover:bg-orange-400" />
         </>
     )
 }
