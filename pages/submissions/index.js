ed6cf4ce10c5da68d21ea6b45df0e67ef33a0fd4
Nevin Tay <nevintay@gmail.com>
2022-08-23 13:54:40 +0800

    feat: import BackButton component into page

--- pages/submissions/index.js
+++ pages/submissions/index.js
@@ -1,7 +1,10 @@
+import BackButton from "../../components/UI/BackButton"
+
 const Submissions = () => {
     return (
         <>
-            <h1 className='w-4/6 text-white text-6xl leading-normal mb-5 text-purple-500 '>Submissions</h1>
+            <h1 className='w-4/6 text-white text-6xl leading-normal mb-5 text-purple-500'>Submissions</h1>
+            <BackButton hoverStyling="hover:bg-purple-400" />
         </>
     )
 }
