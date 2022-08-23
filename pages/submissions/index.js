b0ba2bb62588c38b54672a8e19bc43a7b007ce34
Nevin Tay <nevintay@gmail.com>
2022-08-23 14:22:07 +0800

    feat: import and render submission names

--- pages/submissions/index.js
+++ pages/submissions/index.js
@@ -1,10 +1,16 @@
 import BackButton from "../../components/UI/BackButton"
+import List from "../../components/UI/Layout/List"
+
+import data from "../../utils/SUBMISSIONS.json"
 
 const Submissions = () => {
+    const submissions = data.submissions
+    console.log("subs", submissions)
     return (
         <>
             <h1 className='w-4/6 text-white text-6xl leading-normal mb-5 text-purple-500'>Submissions</h1>
             <BackButton hoverStyling="hover:bg-purple-400" />
+            <List data={submissions} />
         </>
     )
 }
