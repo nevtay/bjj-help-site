61dc103556a5b136f8b3b9d20c39aa911dc4149d
Nevin Tay <nevintay@gmail.com>
2022-08-24 12:49:28 +0800

    feat: allow for custom styles on hover

--- components/UI/Layout/ListItem.js
+++ components/UI/Layout/ListItem.js
@@ -3,10 +3,11 @@ import Link from 'next/link'
 const ListItem = (props) => {
     const item = props.item || ""
     const type = props.type || ""
+    const hoverStyling = props.hoverStyling || ""
 
     return (
         <Link href={`/${type}/${item}`}>
-            <p className='cursor-pointer text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-7 md-5 px-5 py-1 text-center border rounded-full'>{item}</p>
+            <p className={`cursor-pointer text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-7 md-5 px-5 py-1 text-center border rounded-full ${hoverStyling}`}>{item}</p>
         </Link >
     )
 }
