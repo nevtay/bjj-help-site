410784a391b5d965076b6568ac28d5a14fca2c99
Nevin Tay <nevintay@gmail.com>
2022-08-24 12:52:34 +0800

    feat: update layout of selected position page

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -5,7 +5,10 @@ const SelectedPosition = () => {
     const selectedPosition = router.query.selectedPosition || ""
 
     return (
-        <div className="text-white">{selectedPosition}</div>
+        <div>
+            <h1 className='w-4/6 text-6xl leading-normal mb-1 text-yellow-500'>Positions</h1>
+            <h2 className='w-4/6 text-4xl leading-normal text-yellow-300'>{selectedPosition}</h2>
+        </div>
     )
 }
 
