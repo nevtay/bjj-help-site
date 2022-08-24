c25418d32be8fb2bb33c4b5350241319c17c4119
Nevin Tay <nevintay@gmail.com>
2022-08-24 12:36:34 +0800

    feat: update layout so cursor pointer shows correctly when hovering over a list item

--- components/UI/Layout/ListItem.js
+++ components/UI/Layout/ListItem.js
@@ -5,9 +5,9 @@ const ListItem = (props) => {
     const type = props.type || ""
 
     return (
-        <div className='text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-7 md-5 px-5 py-1 text-center border rounded-full'>
-            <Link href={`/${type}/${item}`}>{item}</Link>
-        </div>
+        <Link href={`/${type}/${item}`}>
+            <p className='cursor-pointer text-white first-of-type:ml-0 last-of-type:mr-0 mr-3 mt-7 md-5 px-5 py-1 text-center border rounded-full'>{item}</p>
+        </Link >
     )
 }
 
