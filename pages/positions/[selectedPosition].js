b6b6214825e854d9b174ad0773659ccc0381b5ee
Nevin Tay <nevintay@gmail.com>
2022-08-24 13:06:55 +0800

    feat: add back button with navigation functionality to each position page

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -1,13 +1,15 @@
 import { useRouter } from "next/router"
+import BackButton from "../../components/UI/BackButton"
 
 const SelectedPosition = () => {
     const router = useRouter()
     const selectedPosition = router.query.selectedPosition || ""
-
+    const navigateToPositionsPage = () => router.push('/positions')
     return (
         <div>
             <h1 className='w-4/6 text-6xl leading-normal mb-1 text-yellow-500'>Positions</h1>
-            <h2 className='w-4/6 text-4xl leading-normal text-yellow-300'>{selectedPosition}</h2>
+            <h2 className='w-4/6 text-4xl leading-normal text-yellow-300 mb-5'>{selectedPosition}</h2>
+            <BackButton onBack={navigateToPositionsPage} />
         </div>
     )
 }
