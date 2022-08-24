f4688cad962acadc19a3d800fdc4e8ec95bd8373
Nevin Tay <nevintay@gmail.com>
2022-08-24 12:57:14 +0800

    feat: add prop to <BackButton /> for easier navigation configuration

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -1,14 +1,17 @@
+import { useRouter } from "next/router"
 import BackButton from "../../components/UI/BackButton"
 import List from "../../components/UI/Layout/List"
 
 import data from "../../utils/POSITIONS.json"
 
 const Positions = () => {
+    const router = useRouter()
     const positions = data.positions
+    const navigateToHomePageHandler = () => router.push("/")
     return (
         <>
             <h1 className='w-4/6 text-white text-6xl leading-normal mb-3 text-yellow-500'>Positions</h1>
-            <BackButton hoverStyling="hover:bg-orange-400" />
+            <BackButton hoverStyling="hover:bg-orange-400" onBack={navigateToHomePageHandler} />
             <List data={positions} dataType="positions" hoverStyling="hover:bg-yellow-700" />
         </>
     )
--- pages/submissions/index.js
+++ pages/submissions/index.js
@@ -1,14 +1,17 @@
+import { useRouter } from "next/router"
 import BackButton from "../../components/UI/BackButton"
 import List from "../../components/UI/Layout/List"
 
 import data from "../../utils/SUBMISSIONS.json"
 
 const Submissions = () => {
+    const router = useRouter()
     const submissions = data.submissions
+    const navigateToHomePageHandler = () => router.push("/")
     return (
         <>
             <h1 className='w-4/6 text-6xl leading-normal mb-3 text-purple-500'>Submissions</h1>
-            <BackButton hoverStyling="hover:bg-purple-400" />
+            <BackButton hoverStyling="hover:bg-purple-400" onBack={navigateToHomePageHandler} />
             <List data={submissions} dataType="submissions" hoverStyling="hover:bg-purple-700" />
         </>
     )
