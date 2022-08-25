553e21a3de5332d744d51c3f9aaf81482a2c98d7
Nevin Tay <nevintay@gmail.com>
2022-08-25 15:46:16 +0800

    feat: update and add layouts to match new structure and contents from data source

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -1,15 +1,29 @@
 import { useRouter } from "next/router"
+import positions from "../../utils/POSITIONS.json"
 import BackButton from "../../components/UI/BackButton"
 
 const SelectedPosition = () => {
     const router = useRouter()
     const selectedPosition = router.query.selectedPosition || ""
+    const selectedPositionData = positions.data.find(pos => pos.name.toLowerCase() === selectedPosition.toLowerCase())
+    let { name, description, imagesUrl } = selectedPositionData
+
+    let images = <span className="mt-10 w-5/6 flex flex-row flex-wrap justify-start">
+        {imagesUrl && imagesUrl.map(img => {
+            return (
+                <img src={`${img}`} key={img} className="mr-3 mt-3 md:mt-0 last-child:mr-0 w-auto max-w-sm h-min-content" />
+            )
+        })}
+    </span>
+
     const navigateToPositionsPage = () => router.push('/positions')
     return (
         <div>
             <h1 className='w-4/6 text-6xl leading-normal mb-1 text-yellow-500'>Positions</h1>
-            <h2 className='w-4/6 text-4xl leading-normal text-yellow-300 mb-5'>{selectedPosition}</h2>
-            <BackButton onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black" />
+            <BackButton onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black mb-5" />
+            <h2 className='w-4/6 text-4xl leading-normal text-yellow-300 mb-5'>{name}</h2>
+            <p className='w-5/6 md:w-4/6 text-small leading-normal text-yellow-300 mb-5'>{description}</p>
+            {images}
         </div>
     )
 }
