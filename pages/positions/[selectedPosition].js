a817ee0d5d86b18cb976d188edb9daaf3dc7d819
Nevin Tay <nevintay@gmail.com>
2022-08-26 16:15:45 +0800

    feat: update layout for individual position pages

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -8,10 +8,10 @@ const SelectedPosition = () => {
     const selectedPositionData = positions.data.find(pos => pos.name.toLowerCase() === selectedPosition.toLowerCase())
 
     if (selectedPositionData) {
-        let images = <span className="mt-10 w-5/6 flex flex-row flex-wrap justify-start">
+        let images = <span className="flex flex-col flex-wrap items-center w-12/12 lg:flex-row lg:w-12/12 lg:items-stretch lg:justify-center">
             {selectedPositionData.imagesUrl && selectedPositionData.imagesUrl.map(img => {
                 return (
-                    <img src={`${img}`} key={img} className="mr-3 mt-3 md:mt-0 last-child:mr-0 w-auto max-w-sm h-min-content" />
+                    <img src={`${img}`} key={img} className="rounded-lg object-fill mb-3 w-10/12 md:w-8/12 lg:w-5/12 lg:mt-0 lg:mr-3 last-of-type:lg:mr-0" />
                 )
             })}
         </span>
@@ -22,7 +22,7 @@ const SelectedPosition = () => {
                 <h1 className='w-4/6 text-6xl leading-normal mb-1 text-yellow-500'>Positions</h1>
                 <BackButton onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black mb-5" />
                 <h2 className='w-4/6 text-4xl leading-normal text-yellow-300 mb-5'>{selectedPositionData.name}</h2>
-                <p className='w-5/6 md:w-4/6 text-small leading-normal text-yellow-300 mb-5'>{selectedPositionData.description}</p>
+                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5 md:w-6/6 '>{selectedPositionData.description}</p>
                 {images}
             </div>
         )
