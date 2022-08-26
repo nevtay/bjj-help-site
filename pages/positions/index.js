f17ccc5d2f5b2975d1039d923e4eb5970dfb7c2c
Nevin Tay <nevintay@gmail.com>
2022-08-26 15:10:07 +0800

    fix: remove conflicting css class

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -10,7 +10,7 @@ const Positions = () => {
     const navigateToHomePageHandler = () => router.push("/")
     return (
         <>
-            <h1 className='w-4/6 text-white text-6xl leading-normal mb-3 text-yellow-500'>Positions</h1>
+            <h1 className='w-4/6 text-6xl leading-normal mb-3 text-yellow-500'>Positions</h1>
             <BackButton hoverStyling="hover:bg-orange-400" onBack={navigateToHomePageHandler} />
             <List data={positions} dataType="positions" hoverStyling="hover:bg-orange-400" />
         </>
