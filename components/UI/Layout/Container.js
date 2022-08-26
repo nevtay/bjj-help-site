149420e84e21c1d083dc8617d8ba4de0db5e8851
Nevin Tay <nevintay@gmail.com>
2022-08-26 15:57:30 +0800

    feat: improve padding management by having separate classes for each axis

--- components/UI/Layout/Container.js
+++ components/UI/Layout/Container.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = (props) => {
     return (
-        <div className='p-10 md:p-16 lg:p-24 '>{props.children}</div>
+        <div className='pt-16 px-10 md:px-16 lg:px-24 xl:px-36'>{props.children}</div>
     )
 }
 
