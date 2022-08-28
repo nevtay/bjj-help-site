92a5226e2d6a6199aa4de43380da7a12584cab15
Nevin Tay <nevintay@gmail.com>
2022-08-28 15:56:50 +0800

    feat: replace default <img> elements with nextJS <Image /> component

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -1,3 +1,4 @@
+import Image from 'next/future/image'
 import { useRouter } from "next/router"
 import positions from "../../utils/POSITIONS.json"
 import BackButton from "../../components/UI/BackButton"
@@ -11,9 +12,12 @@ const SelectedPosition = () => {
 
     if (selectedPositionData) {
         let images = <span className="flex flex-col flex-wrap items-start items-center w-12/12 md:items-start lg:flex-row lg:w-12/12 lg:items-stretch lg:justify-start">
-            {selectedPositionData.imagesUrl && selectedPositionData.imagesUrl.map(img => {
+            {selectedPositionData.imagesUrl && selectedPositionData.imagesUrl.map((img, idx) => {
                 return (
-                    <img src={`${img}`} key={img} className="rounded-lg object-fill mb-3 w-12/12 md:w-5/6 lg:w-5/12 lg:mt-0 lg:mr-3 last-of-type:lg:mr-0" />
+                    <img src={img} key={img}
+                        alt={`${idx}`}
+                        placeholder="blur"
+                        className="rounded-lg object-fill mb-3 w-12/12 md:w-5/6 lg:w-5/12 lg:mt-0 lg:mr-3 last-of-type:lg:mr-0" />
                 )
             })}
         </span>
