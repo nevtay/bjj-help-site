714f07384c546d1a6cd8e7972f222767c27b0aa5
Nevin Tay <nevintay@gmail.com>
2022-08-31 16:57:44 +0800

    refactor: remove unused import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
 import Container from '../components/UI/Layout/Container';
-import { data } from 'autoprefixer';
 
 function MyApp({ Component, pageProps }) {
   return (
