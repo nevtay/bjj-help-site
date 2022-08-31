9846c89af75da8c9e73a53b725368d729f3229ff
Nevin Tay <nevintay@gmail.com>
2022-08-31 16:08:22 +0800

    feat: remove additional styling from sort button

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -24,7 +24,7 @@ const Positions = (props) => {
             <h1 className='w-4/6 text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>Positions</h1>
             <Button hoverStyling="hover:bg-orange-400 mb-7" onBack={navigateToHomePageHandler} />
             <br />
-            <Button text={`${sorting === 'asc' ? 'Sort Descending ↓' : 'Sort Ascending ↑'}`} hoverStyling={"bg-yellow-600"} onBack={sortPositions} />
+            <Button text={`${sorting === 'asc' ? 'Sort Descending ↓' : 'Sort Ascending ↑'}`} onBack={sortPositions} />
             <List data={positions} dataType="positions" hoverStyling="hover:bg-orange-400" />
         </>
     )
