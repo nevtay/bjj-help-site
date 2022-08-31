5db7dcba3ae5b4c9dda38673b28472f1c8c4ff6d
Nevin Tay <nevintay@gmail.com>
2022-08-31 16:55:59 +0800

    feat: add footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
 import Container from '../components/UI/Layout/Container';
+import { data } from 'autoprefixer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Container>
-      <Component {...pageProps} />
-    </Container>
+    <div className='flex flex-col justify-between h-screen'>
+      <Container classes={"pt-16"}>
+        <Component {...pageProps} />
+      </Container>
+      <footer className='bg-gradient-to-r from-black via-black to-black text-center text-white bottom-0 left-0 mt-auto w-full py-4'>
+        <Container>
+          <div className='flex flex-col items-center flex-wrap justify-center'>
+            <h1 className='text-sm mb-2'>A Simple Brazilian Jit-Jitsu Resource Site</h1>
+            <h1 className='text-sm mb-1'>nevintay@hotmail.com | {new Date().toLocaleString('default', { year: "numeric" })}</h1>
+          </div>
+        </Container>
+      </footer>
+    </div>
   )
 }
 
