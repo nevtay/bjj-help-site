347e538c569bb301382a37a3bf9dcc882b7a5946
Nevin Tay <nevintay@gmail.com>
2022-08-31 16:01:59 +0800

    feat: add sorting button to sort positions alphabetically

--- pages/positions/index.js
+++ pages/positions/index.js
@@ -1,17 +1,30 @@
+import { useState } from "react"
 import { useRouter } from "next/router"
-import BackButton from "../../components/UI/BackButton"
+import Button from "../../components/UI/Button"
 import List from "../../components/UI/Layout/List"
 
 import data from "../../utils/POSITIONS.json"
 
 const Positions = (props) => {
+    const [sorting, setSorting] = useState('asc')
     const router = useRouter()
     const navigateToHomePageHandler = () => router.push("/")
-    const positions = props.positions
+    let positions = props.positions
+    const sortPositions = () => {
+        if (sorting === 'asc') {
+            positions.sort((a, b) => b.name.localeCompare(a.name))
+            setSorting('desc')
+        } else if (sorting === 'desc') {
+            positions.sort((a, b) => a.name.localeCompare(b.name))
+            setSorting('asc')
+        }
+    }
     return (
         <>
             <h1 className='w-4/6 text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>Positions</h1>
-            <BackButton hoverStyling="hover:bg-orange-400 mb-7" onBack={navigateToHomePageHandler} />
+            <Button hoverStyling="hover:bg-orange-400 mb-7" onBack={navigateToHomePageHandler} />
+            <br />
+            <Button text={`${sorting === 'asc' ? 'Sort Descending ↓' : 'Sort Ascending ↑'}`} hoverStyling={"bg-yellow-600"} onBack={sortPositions} />
             <List data={positions} dataType="positions" hoverStyling="hover:bg-orange-400" />
         </>
     )
@@ -22,7 +35,7 @@ export default Positions
 export async function getStaticProps() {
     return {
         props: {
-            positions: data.positions
+            positions: data.positions,
         }
     }
 }
