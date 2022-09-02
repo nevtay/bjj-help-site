7d12566ee8b7c1d98df1d7dcb14d889a9414ff98
Nevin Tay <nevintay@gmail.com>
2022-09-02 15:47:33 +0800

    feat: initial commit

---
+++ components/UI/Layout/SingleImage.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { PulseLoader } from 'react-spinners'
+
+const SingleImage = (props) => {
+    const [imageLoaded, setImageLoaded] = useState(false)
+    const { image } = props
+    return (
+        <>
+            {!imageLoaded && <PulseLoader className={`justify-center items-center w-12/12 md:w-auto lg:w-6/12 last-of-type:lg:mr-0`} color='white' />}
+            <img
+                src={image}
+                alt={image.toString()}
+                className={`${imageLoaded ? 'block' : 'hidden'} rounded-lg object-fill mb-6 px-1 w-12/12 md:w-auto lg:w-6/12 last-of-type:lg:mr-0`}
+                priority={true}
+                onLoad={() => setImageLoaded(true)}
+            />
+        </>
+    )
+}
+
+export default SingleImage
