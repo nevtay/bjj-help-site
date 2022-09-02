feb16eb01e15c0e3b99d2b45ec4c16aaa3d756ce
Nevin Tay <nevintay@gmail.com>
2022-09-02 13:06:28 +0800

    feat: improve layout and spacing

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -37,17 +37,17 @@ const SelectedPosition = () => {
                         width={1000}
                         height={400}
                         priority={true}
-                        className="rounded-lg object-fill mb-8 w-11/12 md:w-5/6 lg:w-5/12 lg:mt-0 lg:mr-3 last-of-type:lg:mr-0" />
+                        className="rounded-lg object-fill mb-8 px-1 w-12/12 md:w-5/6 lg:w-5/12 lg:mt-0 lg:mr-3 last-of-type:lg:mr-0" />
                 )
             })}
         </span>
         videos =
-            <div className="h-100">
+            <div className="min-h-screen">
                 <h2 className='w-4/6 text-3xl md:text-3xl leading-normal text-yellow-300 mt-20 mb-3'>Videos</h2>
                 {showSpinner && <div className="mt-20 text-center "><PulseLoader color="white" /></div>}
                 <InView
                     as="span"
-                    className="flex flex-col flex-wrap justify-evenly w-6/6 md:w-5/6"
+                    className="flex flex-col flex-wrap justify-evenly w-6/6 md:w-10/12"
                     onChange={(inView) => {
                         if (inView) {
                             setShowSpinner(false)
@@ -59,7 +59,7 @@ const SelectedPosition = () => {
                             key={video}
                             src={video}
                             id={video}
-                            className="rounded-lg mb-10 h-72 md:h-96 w-11/12 mx-auto md:mx-0"
+                            className="rounded-lg mb-10 h-72 md:h-96 w-12/12 mx-1.5 md:mx-0"
                             allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                             title={`A video about ${selectedPositionData.name}`}
