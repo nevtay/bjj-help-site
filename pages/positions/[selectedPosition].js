d5f7fa0bf6711a7ac099eacecc35405d27e90b75
Nevin Tay <nevintay@gmail.com>
2022-09-03 17:32:57 +0800

    refactor: display videos in their respective components

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import { PulseLoader } from 'react-spinners'
-import { InView } from 'react-intersection-observer';
 import Images from '../../components/UI/Layout/Images';
+import Videos from '../../components/UI/Layout/Videos';
 import { useRouter } from "next/router"
 
 import data from "../../utils/POSITIONS.json"
 import Button from "../../components/UI/Button"
 
 const SelectedPosition = () => {
-    const [showSpinner, setShowSpinner] = useState(true)
-
     const router = useRouter()
     const selectedPosition = router.query.selectedPosition || ""
     const selectedPositionData = selectedPosition && data.positions.find(pos => pos.name.toLowerCase() === selectedPosition.toLowerCase())
@@ -30,36 +26,7 @@ const SelectedPosition = () => {
 
     if (selectedPositionData) {
         images = <Images selectedPositionDataImageUrls={selectedPositionData.imagesUrl} />
-        videos =
-            <div className="min-h-screen">
-                <h2 className='w-4/6 text-3xl md:text-3xl leading-normal text-yellow-300 mt-20 mb-3'>Videos</h2>
-                {showSpinner && <div className="mt-20 text-center "><PulseLoader color="white" /></div>}
-                <InView
-                    as="span"
-                    className="flex flex-col flex-wrap justify-evenly w-6/6 md:w-10/12"
-                    onChange={(inView) => {
-                        if (inView) {
-                            setShowSpinner(false)
-                        }
-                    }}
-                >
-                    {selectedPositionData.videosUrl.map(video => {
-                        return <iframe
-                            key={video}
-                            src={video}
-                            id={video}
-                            className="rounded-lg mb-10 h-72 md:h-96 w-12/12 mx-1.5 md:mx-0"
-                            allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                            title={`A video about ${selectedPositionData.name}`}
-                            loading="lazy"
-                        >
-                        </iframe>
-
-                    })
-                    }
-                </InView>
-            </div>
+        videos = <Videos selectedPositionDataVideoUrls={selectedPositionData.videosUrl} />
 
         return (
             <div>
