8016e85bc6e68d0635e5ba96184de3c381999534
Nevin Tay <nevintay@gmail.com>
2022-09-04 11:37:11 +0800

    feat: add title tags and description metadata to relevant sections

--- pages/positions/[selectedPosition].js
+++ pages/positions/[selectedPosition].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router"
+import Head from "next/head";
 import Images from '../../components/UI/Layout/Images';
 import Videos from '../../components/UI/Layout/Videos';
 import data from "../../utils/POSITIONS.json"
@@ -14,11 +15,17 @@ const SelectedPosition = (props) => {
 
     if (!selectedPositionData) {
         return (
-            <div>
-                <h1 className='w-4/6 text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>Positions</h1>
-                <Button onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black mb-7" />
-                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5 md:w-5/6'>{`No data found for "${selectedPosition}"`}</p>
-            </div>
+            <>
+                <Head>
+                    <title>Position not found</title>
+                    <meta name="description" content={`Position data not found!`} />
+                </Head>
+                <div>
+                    <h1 className='w-4/6 text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>Positions</h1>
+                    <Button onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black mb-7" />
+                    <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5 md:w-5/6'>{`No data found for "${selectedPosition}"`}</p>
+                </div>
+            </>
         )
     }
 
@@ -27,14 +34,20 @@ const SelectedPosition = (props) => {
         const videos = <Videos selectedPositionDataVideoUrls={selectedPositionData.videosUrl} />
 
         return (
-            <div>
-                <h1 className='w-4/6 text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>Positions</h1>
-                <Button onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black mb-7" />
-                <h2 className='w-4/6 text-3xl md:text-4xl leading-normal text-yellow-300 mb-5'>{selectedPositionData.name}</h2>
-                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5 md:w-5/6'>{selectedPositionData.description}</p>
-                {images}
-                {videos}
-            </div>
+            <>
+                <Head>
+                    <title>{selectedPositionData.name}</title>
+                    <meta name="description" content={`Learn about ${selectedPositionData.name}`} />
+                </Head>
+                <div>
+                    <h1 className='w-4/6 text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>Positions</h1>
+                    <Button onBack={navigateToPositionsPage} hoverStyling="hover:bg-orange-400 hover:text-black mb-7" />
+                    <h2 className='w-4/6 text-3xl md:text-4xl leading-normal text-yellow-300 mb-5'>{selectedPositionData.name}</h2>
+                    <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5 md:w-5/6'>{selectedPositionData.description}</p>
+                    {images}
+                    {videos}
+                </div>
+            </>
         )
     }
 }
