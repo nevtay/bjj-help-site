cc81fcaced7cc22730aca7c7db09a2c1de098051
Nevin Tay <nevintay@gmail.com>
2022-09-06 14:59:45 +0800

    feat: layout improvements

--- components/UI/Layout/Videos.js
+++ components/UI/Layout/Videos.js
@@ -7,7 +7,7 @@ const Videos = (props) => {
             <h2 className='text-3xl md:text-3xl leading-normal text-yellow-300 mb-5'>Videos</h2>
             {!selectedPositionDataVideoUrls.length && <h1 className='text-white m-auto text-2xl'>No videos available</h1>}
             {selectedPositionDataVideoUrls.length
-                && <span className="flex flex-row flex-wrap gap-6 justify-start md:gap-4">
+                && <span className="flex flex-col flex-wrap items-start lg:w-11/12 lg:flex-row lg:m-auto lg:justify-start lg:h-auto">
                     {selectedPositionDataVideoUrls.map(vid => <SingleVideo key={vid} video={vid} />)}
                 </span>
             }
