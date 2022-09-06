5829929eecdfbc3ccc5b9b50425e03fe15021eaa
Nevin Tay <nevintay@gmail.com>
2022-09-06 15:00:11 +0800

    feat: layout improvements

--- components/UI/Layout/Images.js
+++ components/UI/Layout/Images.js
@@ -8,7 +8,7 @@ const Images = (props) => {
             {!selectedPositionDataImageUrls && <h1 className='text-white m-auto text-2xl'>No images available</h1>}
             {selectedPositionDataImageUrls
                 && selectedPositionDataImageUrls.length
-                && <span className="flex items-center flex-col flex-wrap gap-2 md:gap-4 md:flex-row md:w-5/6 md:justify-around">
+                && <span className="px-2 flex flex-col flex-wrap justify-start md:flex-row md:w-6/6">
                     {selectedPositionDataImageUrls && selectedPositionDataImageUrls.map(img => <SingleImage key={img} image={img} />)}
                 </span>
             }
