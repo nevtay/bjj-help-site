7649da6e52f79feea0c14d04eb745fdc69d4a8a6
Nevin Tay <nevintay@gmail.com>
2022-09-06 14:59:59 +0800

    feat: layout improvements

--- components/UI/Layout/SingleVideo.js
+++ components/UI/Layout/SingleVideo.js
@@ -14,8 +14,8 @@ const SingleVideo = (props) => {
                 }
             }}
             rootMargin="0px"
-            threshold="0"
-            className='m-auto md:m-0'
+            threshold="0.2"
+            className='h-72 px-3 py-5 w-full m-auto md:w-11/12 md:h-80 lg:w-6/12 lg:min-h-full 2xl:min-h-full lg:mx-0'
         >
             {!videoLoaded &&
                 <div className='flex justify-center h-80'>
@@ -25,7 +25,7 @@ const SingleVideo = (props) => {
                 loading='lazy'
                 src={video}
                 id={video}
-                className={`rounded-lg w-full w-auto h-96`}
+                className={`rounded-lg w-full h-full`}
                 allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             >
