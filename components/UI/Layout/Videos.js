8ba6f9c50e5b53bf4a21183cb4525f159d295397
Nevin Tay <nevintay@gmail.com>
2022-10-02 16:12:06 +0800

    feat: make minor layout changes and refactor css

--- components/UI/Layout/Videos.js
+++ components/UI/Layout/Videos.js
@@ -3,11 +3,11 @@ import SingleVideo from "./SingleVideo"
 const Videos = (props) => {
     const { selectedPositionDataVideoUrls } = props
     return (
-        <div className="min-h-screen">
-            <h2 className='text-3xl md:text-3xl leading-normal text-yellow-300 mb-5'>Videos</h2>
+        <div>
+            <h2 className='text-3xl leading-normal text-yellow-300 mb-5 md:text-3xl'>Videos</h2>
             {!selectedPositionDataVideoUrls.length && <h1 className='text-white m-auto text-2xl'>No videos available</h1>}
             {selectedPositionDataVideoUrls.length
-                && <span className="flex flex-col flex-wrap items-start lg:w-11/12 lg:flex-row lg:m-auto lg:justify-start lg:h-auto">
+                && <span className="flex flex-col flex-wrap mx-auto lg:w-12/12 lg:flex-row">
                     {selectedPositionDataVideoUrls.map(vid => <SingleVideo key={vid} video={vid} />)}
                 </span>
             }
