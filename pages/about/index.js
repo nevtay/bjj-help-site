7f3133b7a6f41b8fd7d47878088436bcb1684502
Nevin Tay <nevintay@gmail.com>
2022-10-16 13:41:46 +0800

    feat: update content

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,10 +10,13 @@ const index = () => {
             </Head>
             <div className="w-6/6 mb-20">
                 <h1 className='text-5xl md:text-6xl leading-normal mb-5 font-bold tracking-wide text-yellow-500'>What is Brazilian Jiu-Jitsu?</h1>
-                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5'>{`Brazilian Jiu-Jitsu (or simply "BJJ") is a martial art. Specifically, it's a predominantly ground-based grappling combat system with defensive and offensive applications that maximises survival.`}</p>
+                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5'>{`Brazilian Jiu-Jitsu (or simply "BJJ") is a martial art that combines grappling with ground fighting to control and defeat opponents through dominant positioning and submissions.`}</p>
 
                 <h1 className='text-5xl md:text-6xl leading-normal mt-10 mb-5 font-bold tracking-wide text-yellow-500'>Origins</h1>
-                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5'>"Modern" BJJ could be said to have originated in the late 1800s, when Mitsuyo Maeda took his lessons from <a href="https://en.wikipedia.org/wiki/Kan%C5%8D_Jigor%C5%8D"><u>Kanō Jigorō</u></a>, the founder of Judo, and tested them against challengers overseas before arriving in Brazil in 1914. There, he would enter a working relationship with Gastao Gracie, a businessman who was pivotal in helping Maeda establish a local Japanese community. In return Maeda offered to teach Jiu-Jitsu to Gastao's oldest son, Carlos, and Carlos would eventually proceed pass on those teachings to his brothers, including one Helios Gracie.</p>
+
+                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5'>Mitsuyo Maeda, one of <a href="https://en.wikipedia.org/wiki/Kan%C5%8D_Jigor%C5%8D" rel='noreferrer' target={'_blank'}><u>Kanō Jigorō's</u></a> finest students, arrived in Brazil in 1914 after a hugely successful career promoting Judo and jiu-jitsu overseas in demonstrations and hundreds of fights against all types of challengers.</p>
+
+                <p className='w-12/12 text-small leading-normal text-yellow-300 mb-5'>During his stay in Brazil, he befriended Gastao Gracie, a local businessman who assisted Maeda with settling into the country and helped develop the local Japanese community. In return, Maeda offered to teach Jiu-Jitsu to Gastao's oldest son, Carlos Gracie. His four brothers would eventually join him on the mats, of which one would become the most important character in modern BJJ: <a href="https://en.wikipedia.org/wiki/Kan%C5%8D_Jigor%C5%8D" rel='noreferrer' target={'_blank'}><u>Helio Gracie</u></a>.</p>
             </div>
         </>
     )
