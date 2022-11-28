1ad745f07cdc787f850813d24f779a0b722d4d72
Nevin Tay <nevintay@gmail.com>
2022-11-28 12:57:38 +0800

    feat: initial commit of new shared component

---
+++ components/UI/SectionButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SectionButton = (props) => {
+
+    return (
+        <button onClick={() => props.navigateTo()} type="button" className={`tracking-wide text-xl inline-flex items-center justify-center border-2 rounded-full px-5 py-3 font-bold w-auto hover:underline ${props.className}`} aria-expanded="false">
+            {props.pageName}
+        </button>
+    )
+}
+
+export default SectionButton
