afe4841616a94123e83026e2d700a7f35727ca5c
Nevin Tay <nevintay@gmail.com>
2024-03-29 23:46:16 +0800

    feat: add button to allow users to visit the 'guards' page

--- components/Hero.js
+++ components/Hero.js
@@ -5,6 +5,7 @@ const Hero = () => {
     const router = useRouter()
     const navigateToPositionsPage = () => router.push('/positions')
     const navigateToSubmissionsPage = () => router.push('/submissions')
+    const navigateToGuardsPage = () => router.push('/guards')
     const navigateToAboutPage = () => router.push('/about')
     const textStyleClasses = 'w-6/6 mb-5 text-6xl leading-normal text-white md:text-7xl md:leading-relaxed md:w-5/6 xl:w-4/6 '
 
@@ -12,6 +13,7 @@ const Hero = () => {
         <div className='min-h-screen'>
             <h1 className={`${textStyleClasses}`}>A Brazilian Jiu-Jitsu Resource Page</h1>
             <span className='flex flex-col items-start justify-start flex-wrap md:flex-row'>
+                <SectionButton navigateTo={navigateToGuardsPage} pageName={"Guards"} className="border-blue-500 text-blue-500 hover:bg-blue-50 mt-3 md:mr-7 md:mt-0" />
                 <SectionButton navigateTo={navigateToPositionsPage} pageName={"Positions"} className="border-yellow-500 text-yellow-500 hover:bg-yellow-50 mt-0 md:mr-7 sm:mt-0" />
                 <SectionButton navigateTo={navigateToSubmissionsPage} pageName={"Submissions"} className="border-purple-500 text-purple-500 hover:bg-purple-50 mt-3 md:mr-7 md:mt-0" />
             </span>
