f4694f4e3ff5e7ddd9fc88e6a5be699ac486ed11
Nevin Tay <nevintay@gmail.com>
2024-04-20 18:10:00 +0800

    feat: update styling

--- components/UI/Videos.js
+++ components/UI/Videos.js
@@ -7,7 +7,7 @@ const Videos = (props) => {
             <h2 className='text-3xl leading-normal text-yellow-300 mb-5 md:text-3xl'>Videos</h2>
             {!selectedPositionDataVideoUrls.length && <h1 className='text-white m-auto text-2xl'>No videos available</h1>}
             {selectedPositionDataVideoUrls.length
-                && <span className="flex lg:flex-row md:flex-col sm:flex-col flex-wrap justify-around mx-auto lg:w-12/12">
+                && <span className="mx-auto flex flex-col lg:flex-row flex-wrap">
                     {selectedPositionDataVideoUrls.map(vid => <SingleVideo key={vid} video={vid} />)}
                 </span>
             }
