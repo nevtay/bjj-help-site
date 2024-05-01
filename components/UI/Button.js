96c0ed50ae352a8ed65aef67e2de86ce0eff3788
Nevin Tay <nevintay@gmail.com>
2024-05-01 16:27:33 +0800

    feat: update <Button /> component to accept css classes from props

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -2,7 +2,7 @@ const Button = (props) => {
     return (
         <button
             onClick={props.onBack}
-            className={`rounded-full text-white border px-4 py-2 ${props.hoverStyling ? props.hoverStyling : "hover:bg-gray-100"} hover:text-black`}>
+            className={`${props.className} rounded-full text-white border px-4 py-2 ${props.hoverStyling ? props.hoverStyling : "hover:bg-gray-100"} hover:text-black`}>
             <span className="display: table-cell">{props.text || `← Back`}</span>
         </button>
     )
